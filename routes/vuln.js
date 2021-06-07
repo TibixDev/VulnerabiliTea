a80e1529f2d73757161ed11ed4eece5da8c06555
TibixDev <fuloptibi03@gmail.com>
2021-06-07 20:17:29 +0300

    Date token security fixes

--- routes/vuln.js
+++ routes/vuln.js
@@ -584,16 +584,18 @@ router.post(
             text: "There was no VTID specified.",
             type: "noVTID",
         }),
-        body("expiryDate").exists().withMessage({
+        body("expiryDate")
+        .exists()
+        .withMessage({
             text: "There was no token expiry date specified.",
             type: "noTokenExpDate",
-        }),
-        // TODO: Fix check
-        /*.isDate()
-            .withMessage({
-                text: "The expiry date specified was not a valid date.",
-                type: "invalidTokenExpDate",
-            }),*/
+        })
+        .matches(/^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/)
+        .withMessage({
+            text: "The token date specified was in an invalid format.",
+            type: "invalidTokenExpDate",
+        })
+        // TODO: Monitor date check for errors
     ], helpers.processValidationErrs,
     async (req, res) => {
         let vuln = await Vulnerability.findOne(
